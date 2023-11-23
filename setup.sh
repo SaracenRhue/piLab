aade2b0b9e561af510c86f4ca2079e41a90d9ac8
SaracenRhue <77064702+SaracenRhue@users.noreply.github.com>
2023-11-23 12:37:03 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
+# Dependencies
 sudo apt update && sudo apt upgrade -y
 sudo hostnamectl set-hostname pilab
-sudo apt install -y wget git htop dnsmasq 
+sudo apt install -y wget git htop samba dnsmasq 
 curl -fsSL https://get.casaos.io | sudo bash
 curl -fsSL https://tailscale.com/install.sh | sh
+# Configurations
 
+# Services
+
+# Permissions
+sudo smbpasswd -a $(whoami)
 
 sudo tailscale up --advertise-routes=10.10.20.0/24
